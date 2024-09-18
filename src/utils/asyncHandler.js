15a872bab43852c29f6b34cc0b2b21f175a8b45a
harshilpec17 <harshilsuthar1995@gmail.com>
2024-09-18 12:05:25 -0400

    API response handler and i CORS cookie-parser

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+const asyncHandler = (requestHandler) => (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+};
+
+export { asyncHandler };
+
+const asyncHandler2 = (requestHandler) => async (req, res, next) => {
+  try {
+    await requestHandler(req, res, next);
+  } catch (error) {
+    err.status(err.code || 500).json({
+      message: err.message,
+      status: "failed",
+    });
+  }
+};
