709ee9feaeea99a48c1bbcbd2dad08521f623cfa
harshilpec17 <harshilsuthar1995@gmail.com>
2024-09-23 13:11:52 -0400

    added multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+(async function () {
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("file uploaded successfully", response.url());
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the temporary saved file
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
